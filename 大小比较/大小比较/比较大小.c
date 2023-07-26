a321615b4a0c000b1c08b62fb14eb249679c0532
Le_C <2114776682@qq.com>
2023-07-26 20:14:14 +0800

    大小比较

---
+++ 大小比较/大小比较/比较大小.c
@@ -0,0 +1,18 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d%d", &a, &b);
+	if (a > b)
+	{
+		printf("%d", a);
+	}
+	else
+	{
+		printf("%d", b);
+	}
+
+	return 0;
+}
