5aa71c4dee06e264e1597ea9ab7441c02947b858
Le_C <2114776682@qq.com>
2023-07-27 16:08:19 +0800

    常量和宏

---
+++ 2023.7.27常量和宏/2023.7.27常量和宏/7.27常量和宏.c
@@ -0,0 +1,10 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#define MAX 1000
+#define ADD(X,Y) ((X)+(Y))
+#include <stdio.h>
+int main()
+{
+	printf("%d\n", MAX);
+	printf("%d\n", 4 * ADD(2, 3));
+	return 0;
+}
