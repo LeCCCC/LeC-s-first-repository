21e747770cbed76ae824852ff084ef743e2907aa
Le_C <2114776682@qq.com>
2023-08-05 13:46:23 +0800

    1

---
+++ 2023.8.5函数调用/2023.8.5函数调用/8.5.c
@@ -0,0 +1,50 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int is(int i)
+//{
+//	int a;
+//	for (a = 2; a < i; a++)
+//	{
+//		if (i % a == 0)
+//			return 0;
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	int i;
+//	scanf("%d", &i);
+//	
+//		if (is(i) == 1)
+//		{
+//			printf("这是一个素数");
+//		}
+//		else
+//		{
+//			printf("这不是一个素数");
+//		}
+//	End:
+//	return 0;
+//}
+//int is(int y)
+//{
+//	if (y % 4 == 0)
+//		return 1;
+//}
+//int main()
+//{
+//	int year;
+//	scanf("%d", &year);
+//	if (is(year) == 1)
+//		printf("闰年");
+//	else
+//		printf("不是闰年");
+//	return 0;
+//}
+//int main()
+//{
+//	int num,a;
+//	scanf("%d",a)
+//	for(num=0;num < a;)
+//	return 0;
+//}
